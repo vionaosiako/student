9da5d6f08f3c0ff7dde625db9da877e384ac7d9c
vionaosiako <viona.osiako@student.moringaschool.com>
2023-01-23 12:48:03 +0300

    ft: add variables of class student

--- src/main/java/student/student.java
+++ src/main/java/student/student.java
@@ -9,23 +9,5 @@ public class student {
     private LocalDate dob;
     private Integer age;
 
-    public student() {
-    }
-
-    public student(String name, String email, LocalDate dob, Integer age) {
-        this.name = name;
-        this.email = email;
-        this.dob = dob;
-        this.age = age;
-    }
-
-    public student(Long id, String name, String email, LocalDate dob, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.dob = dob;
-        this.age = age;
-    }
-
 
 }
